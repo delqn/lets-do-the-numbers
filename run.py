aa4b7a1d59a07ed7bee8b9a6f230de5fb463e436
Delyan Raychev <delyan@raychev.org>
2019-08-31 01:03:59 -0700

    Runner

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+from ldtn import split, stats, classifier, bow_vector, Predictors, fit, tokenizer
+
+
+train_test = split(
+    size_of_test=0.4,
+    dataset_filename='training_sets/product_reviews.tsv',
+)
+
+print(
+    stats(
+        train_test, Predictors, bow_vector, classifier, fit, tokenizer
+    ))
