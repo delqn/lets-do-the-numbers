04d36b5a42c154f9b78d66cd94866d5fb052944d
Delyan Raychev <delyan@raychev.org>
2019-08-31 11:11:01 -0700

    Ability to persist model

--- ldtn/classifier.py
+++ ldtn/classifier.py
@@ -1,3 +1,5 @@
+import pickle
+
 from sklearn.pipeline import Pipeline
 
 def fit(x_train, y_train, predictors, bow_vector, tokenizer, classifier):
@@ -8,3 +10,6 @@ def fit(x_train, y_train, predictors, bow_vector, tokenizer, classifier):
     ])
     return pipe.fit(x_train, y_train)
 
+def save_model(model, file_name):
+    with open(file_name, 'w') as f:
+        pickle.dump(model, f, protocol=pickle.HIGHEST_PROTOCOL)
